b42f151e372ac0c4d3a50e1906cb6be553081ff2
Oosasukel <rodrigo_gonn@hotmail.com>
2021-04-16 15:39:58 -0300

    Configurações iniciais

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Title, Text } from '../../styles/404/styles';
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Text>OPS...</Text>
+      <Title>Página não encontrada :(</Title>
+    </Container>
+  );
+};
+
+export default NotFound;
