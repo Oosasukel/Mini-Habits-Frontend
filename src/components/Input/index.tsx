44b479712ccaa45dde96600449b70281195c3b02
Oosasukel <rodrigo_gonn@hotmail.com>
2021-05-27 22:16:16 -0300

    Validação com yup

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,48 +1,87 @@
-import { InputHTMLAttributes, LegacyRef, useState } from "react";
-import { ReactSVG } from "react-svg";
-import styles from "./styles.module.scss";
+import { useField } from '@unform/core';
+import {
+  InputHTMLAttributes,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { ReactSVG } from 'react-svg';
+import styles from './styles.module.scss';
 
-interface InputComponentProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
   label: string;
-  inputRef: LegacyRef<HTMLInputElement>;
 }
 
-const InputComponent = ({ label, inputRef, ...rest }: InputComponentProps) => {
+const Input = ({ name, label, style, ...rest }: InputProps) => {
   const [passwordIsVisible, setPasswordIsVisible] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const togglePasswordIsVisible = (event) => {
-    event.preventDefault();
-    setPasswordIsVisible(!passwordIsVisible);
-  };
+  useEffect(() => {
+    registerField<HTMLInputElement>({
+      name: fieldName,
+      ref: inputRef,
+      getValue: (ref) => {
+        return ref.current.value;
+      },
+      setValue: (ref, value) => {
+        ref.current.value = value;
+      },
+      clearValue: (ref) => {
+        ref.current.value = '';
+      },
+    });
+  }, [fieldName, registerField]);
 
-  const handleFocus = () => {
+  const togglePasswordIsVisible = useCallback(
+    (event) => {
+      event.preventDefault();
+      setPasswordIsVisible(!passwordIsVisible);
+    },
+    [passwordIsVisible]
+  );
+
+  const handleFocus = useCallback(() => {
     setIsFocused(true);
-  };
+  }, []);
 
-  const handleBlur = () => {
+  const handleBlur = useCallback(() => {
     setIsFocused(false);
-  };
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <p className={isFocused ? styles.active : ""}>{label}</p>
-      <div className={styles.inputContainer}>
-        <input
-          {...rest}
-          ref={inputRef}
-          type={passwordIsVisible ? "text" : "password"}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        />
-        <ReactSVG
-          className={styles.icon}
-          onMouseDown={togglePasswordIsVisible}
-          src={passwordIsVisible ? "/icons/closeEye.svg" : "/icons/openEye.svg"}
-          alt={passwordIsVisible ? "Ocultar senha" : "Mostrar senha"}
-        />
-      </div>
+    <div style={style} className={styles.container}>
+      <label
+        className={`${styles.label} ${
+          error ? styles.error : isFocused ? styles.active : ''
+        }`}
+      >
+        {label}
+        <div className={styles.inputContainer}>
+          <input
+            {...rest}
+            defaultValue={defaultValue}
+            ref={inputRef}
+            type={passwordIsVisible ? 'text' : 'password'}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+          <ReactSVG
+            className={styles.icon}
+            onMouseDown={togglePasswordIsVisible}
+            src={
+              passwordIsVisible ? '/icons/closeEye.svg' : '/icons/openEye.svg'
+            }
+            alt={passwordIsVisible ? 'Ocultar senha' : 'Mostrar senha'}
+          />
+        </div>
+      </label>
+      {error && <label className={styles.errorLabel}>{error}</label>}
     </div>
   );
 };
 
-export default InputComponent;
+export default Input;
--- src/pages/redefinir-senha/[requestId]/[requestSecret]/index.tsx
+++ src/pages/redefinir-senha/[requestId]/[requestSecret]/index.tsx
@@ -1,28 +1,61 @@
 import styles from './styles.module.scss';
 import { useRouter } from 'next/router';
+import Lottie from 'react-lottie';
+import { Form } from '@unform/web';
+import * as yup from 'yup';
 
-import React, { useRef } from 'react';
-import InputComponent from '../../../../components/Input';
+import React, { useRef, useState } from 'react';
+import Input from '../../../../components/Input';
 import api from '../../../../services/api';
+import Button from '../../../../components/Button';
+import logoAnimation from '../../../../assets/lotties/Logo.json';
+
+interface NewPasswordData {
+  password: string;
+  confirmPassword: string;
+}
 
 const NewPassword = () => {
   const router = useRouter();
   const { requestId, requestSecret } = router.query;
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const formRef = useRef(null);
 
-  const inputPassword = useRef<HTMLInputElement>(null);
-  const inputPasswordConfirm = useRef<HTMLInputElement>(null);
+  const handleSubmit = async ({
+    password,
+    confirmPassword,
+  }: NewPasswordData) => {
+    setError('');
+    setLoading(true);
+    formRef.current.setErrors({});
 
-  const handleConfirm = () => {
-    const password = inputPassword.current.value;
-    const confirmPassword = inputPasswordConfirm.current.value;
+    const schema = yup.object().shape({
+      password: yup.string().required('Senha é obrigatória.'),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], 'As senhas devem ser iguais.')
+        .required('Senha é obrigatória'),
+    });
 
-    if (!password) {
-      console.log('A senha deve ser preenchida');
-      return;
-    }
+    try {
+      await schema.validate(
+        { password, confirmPassword },
+        {
+          abortEarly: false,
+        }
+      );
+    } catch (error) {
+      const validationErrors = {};
 
-    if (password !== confirmPassword) {
-      console.log('as senhas devem ser iguais');
+      if (error instanceof yup.ValidationError) {
+        error.inner.forEach((currentError) => {
+          validationErrors[currentError.path] = currentError.message;
+        });
+        formRef.current.setErrors(validationErrors);
+      }
+
+      setLoading(false);
       return;
     }
 
@@ -32,41 +65,61 @@ const NewPassword = () => {
         requestSecret,
         password,
       })
-      .then((response) => {
-        console.log('deu certo');
-        console.log(response);
+      .then(() => {
+        router.push('/senha-alterada');
       })
       .catch((error) => {
-        console.log('deu erro no Backend');
-        console.log(error);
+        const currentStatus = error.response.status;
+        const expectedErrorsStatus = [400, 401, 404];
+
+        if (expectedErrorsStatus.includes(currentStatus)) {
+          setError('Requisição inválida.');
+        } else {
+          setError('Algo inesperado aconteceu. Tente novamente mais tarde.');
+        }
+
+        setLoading(false);
       });
   };
 
   return (
     <div className={styles.container}>
-      <img
-        className={styles.logo}
-        title="Logo"
-        src="/images/logo.png"
-        alt="Logo"
-      />
-      <h1>Digite uma nova senha</h1>
+      <div className={styles.content}>
+        <div className={styles.animationContainer}>
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: logoAnimation,
+            }}
+            isClickToPauseDisabled={true}
+          />
+        </div>
 
-      <InputComponent
-        inputRef={inputPassword}
-        placeholder="Senha"
-        label="Senha"
-      />
+        <h1>Digite uma nova senha</h1>
 
-      <InputComponent
-        inputRef={inputPasswordConfirm}
-        placeholder="Confirmar senha"
-        label="Confirmar senha"
-      />
+        <Form ref={formRef} onSubmit={handleSubmit} style={{ width: '100%' }}>
+          <Input
+            name="password"
+            placeholder="Senha"
+            label="Senha"
+            style={{ marginBottom: '1rem' }}
+          />
 
-      <button className="button" onClick={handleConfirm}>
-        Confirmar
-      </button>
+          <Input
+            name="confirmPassword"
+            placeholder="Confirmar senha"
+            label="Confirmar senha"
+            style={{ marginBottom: '1rem' }}
+          />
+
+          {error && <p className={styles.error}>{error}</p>}
+
+          <Button loading={loading} style={{ marginTop: '1rem' }}>
+            Confirmar
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
